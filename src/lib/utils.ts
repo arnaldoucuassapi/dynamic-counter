776c507453c4ec6f866ba5241157950f78c35cd1
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-08-17 03:52:03 +0100

    added ui components

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function changeDarkMode() {
+  document.documentElement.classList.toggle('dark');
+}
